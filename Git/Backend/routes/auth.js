b6b3d269c8342300c8fd5d2dcaeb665191d4b790
PrathameshJoshi123 <165560967+PrathameshJoshi123@users.noreply.github.com>
2024-10-02 11:27:48 +0530

    Update auth.js

--- Git/Backend/routes/auth.js
+++ Git/Backend/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { register, login } = require('../controllers/authController');
+const { register, login, getUser } = require('../controllers/authController');
+const authMiddleware = require('../middleware/authMiddleware')
 
 // Register
 router.post('/register', register);
@@ -8,6 +9,7 @@ router.post('/register', register);
 // Login
 router.post('/login', login);
 
+router.get('/user', authMiddleware, getUser);
 module.exports = router;
 
 
